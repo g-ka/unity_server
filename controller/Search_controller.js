96eb1349764b77ee1676509e4033c70194617c77
g-ka <gkaphilathithiya@gmail.com>
2023-12-10 07:09:25 +0530

    search_update

--- controller/Search_controller.js
+++ controller/Search_controller.js
@@ -2,6 +2,9 @@ const professionals = require('../model/Professionals');
 
 const search_handler = async (req, res) =>
 {
+  const user_id = req.cookies?.id;
+  if(!user_id) return res.sendStatus(401);
+
   const { search, page_number } = req.body;
 
   if(search === '')
@@ -26,7 +29,16 @@ const search_handler = async (req, res) =>
 
   const search_list = await professionals.find({ id: { $in: filtered_id } });
 
-  return res.status(200).json({ search_list });
+  // INSERTING UPDATES:
+  const { updated_professionals } = await users.findOne({ id: user_id });
+  const updated_id = updated_professionals.map(professional => professional.id);
+  
+  const filtered_professional_list = search_list.filter(professional => !updated_id.includes(professional.id));
+  
+  const final_list = [...filtered_professional_list, ...updated_professionals];
+  const sorted_final_list = final_list.sort((a, b) => a.id - b.id);
+
+  return res.status(200).json({ search_list: sorted_final_list });
 };
 
 module.exports = { search_handler }
