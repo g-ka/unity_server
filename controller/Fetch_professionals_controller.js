fe60672e250b35d7ce4fb60d42100fda95a0fb76
g-ka <gkaphilathithiya@gmail.com>
2023-12-15 22:12:33 +0530

    update_crud

--- controller/Fetch_professionals_controller.js
+++ controller/Fetch_professionals_controller.js
@@ -8,58 +8,68 @@ const fetch_professionals_handler = async (req, res) =>
   let { page_number, domain, gender, avail } = req.query;
   let professionals_list = [];
 
-  if(domain === 'All' && gender === 'All' && avail === 'All')
-  professionals_list = await professionals.find().skip((page_number-1)*10).limit(20);
-  else
-  {
-    if(domain !== 'All' && gender === 'All' && avail === 'All') 
-    professionals_list = await professionals.find({ $and: [{ domain: domain }] }).skip((page_number-1)*10).limit(20);
-    else if(domain === 'All' && gender !== 'All' && avail === 'All')
-    professionals_list = await professionals.find({ $and: [{ gender: gender }] }).skip((page_number-1)*10).limit(20);
-    else if(domain === 'All' && gender === 'All' && avail !== 'All')
-    {
-      if(avail === 'Available') avail = true;
-      else avail = false
-
-      professionals_list = await professionals.find({ $and: [{ available: avail }] }).skip((page_number-1)*10).limit(20);
-    }    
-    if(domain !== 'All' && gender !== 'All' && avail === 'All') 
-    professionals_list = await professionals.find({ $and: [{ domain: domain }, { gender: gender }] }).skip((page_number-1)*10).limit(20);
-    if(domain == 'All' && gender !== 'All' && avail !== 'All') 
-    {
-      if(avail === 'Available') avail = true;
-      else avail = false
-
-      professionals_list = await professionals.find({ $and: [{ gender: gender }, { available: avail }] }).skip((page_number-1)*10).limit(20);
-    }  
-    if(domain !== 'All' && gender === 'All' && avail !== 'All') 
-    {
-      if(avail === 'Available') avail = true;
-      else avail = false
-
-      professionals_list = await professionals.find({ $and: [{ domain: domain }, { available: avail }] }).skip((page_number-1)*10).limit(20);
-    }    
-    if(domain !== 'All' && gender !== 'All' && avail !== 'All')
-    {
-      if(avail === 'Available') avail = true;
-      else avail = false
-
-      professionals_list = await professionals.find({ $and: [{ domain: domain }, { gender: gender }, { available: avail }] }).skip((page_number-1)*10).limit(20);
-    }    
-  }  
+  const original_professionals_list = await professionals.find();
 
   // INSERTING UPDATES:
   const { updated_professionals } = await users.findOne({ id: user_id });
   const updated_id = updated_professionals.map(professional => professional.id);
-  const professional_list_id = professionals_list.map(professional => professional.id);
+  const professional_list_id = original_professionals_list.map(professional => professional.id);
   
-  const filtered_professional_list = professionals_list.filter(professional => !updated_id.includes(professional.id));
+  const filtered_professional_list = original_professionals_list.filter(professional => !updated_id.includes(professional.id));
   const filtered_updated_professionals = updated_professionals.filter(professional => professional_list_id.includes(professional.id));
   
   const final_list = [...filtered_professional_list, ...filtered_updated_professionals];
-  const sorted_final_list = final_list.sort((a, b) => a.id - b.id);
+  const udpated_professionals_list = final_list.sort((a, b) => a.id - b.id);
 
-  return res.status(200).json({ professionals_list: sorted_final_list })
+  if(domain === 'All' && gender === 'All' && avail === 'All')
+  {
+    professionals_list = udpated_professionals_list.slice((page_number - 1)*10, (page_number)*20);
+  }  
+  else
+  {
+    if(domain !== 'All' && gender === 'All' && avail === 'All') 
+    {      
+      professionals_list = udpated_professionals_list.filter(professional => professional.domain === domain).slice((page_number - 1)*10, (page_number)*20);
+    }    
+    else if(domain === 'All' && gender !== 'All' && avail === 'All')
+    {
+      professionals_list = udpated_professionals_list.filter(professional => professional.gender === gender).slice((page_number - 1)*10, (page_number)*20);
+    }    
+    else if(domain === 'All' && gender === 'All' && avail !== 'All')
+    {
+      if(avail === 'Available') avail = true;
+      else avail = false
+      
+      professionals_list = udpated_professionals_list.filter(professional => professional.available === avail).slice((page_number - 1)*10, (page_number)*20);      
+    }    
+    else if(domain !== 'All' && gender !== 'All' && avail === 'All') 
+    {      
+      professionals_list = udpated_professionals_list.filter(professional => professional.domain === domain && professional.gender === gender).slice((page_number - 1)*10, (page_number)*20);      
+    }    
+    else if(domain == 'All' && gender !== 'All' && avail !== 'All') 
+    {
+      if(avail === 'Available') avail = true;
+      else avail = false
+      
+      professionals_list = udpated_professionals_list.filter(professional => professional.gender === gender && professional.available === avail).slice((page_number - 1)*10, (page_number)*20);      
+    }  
+    else if(domain !== 'All' && gender === 'All' && avail !== 'All') 
+    {
+      if(avail === 'Available') avail = true;
+      else avail = false
+      
+      professionals_list = udpated_professionals_list.filter(professional => professional.domain === domain && professional.available === avail).slice((page_number - 1)*10, (page_number)*20);      
+    }    
+    else if(domain !== 'All' && gender !== 'All' && avail !== 'All')
+    {
+      if(avail === 'Available') avail = true;
+      else avail = false
+      
+      professionals_list = udpated_professionals_list.filter(professional => professional.domain === domain && professional.gender === gender && professional.available === avail).slice((page_number - 1)*10, (page_number)*20);      
+    }    
+  }  
+
+  return res.status(200).json({ professionals_list })
 };
 
 const fetch_professional_handler = async (req, res) =>
@@ -73,6 +83,7 @@ const fetch_professional_handler = async (req, res) =>
   const { updated_professionals } = await users.findOne({ id: user_id });
 
   const exist = updated_professionals.find(professional => professional.id == id);
+  
   if(exist) return res.status(200).json({ professional: exist });
 
   const professional = await professionals.findOne({ id });
